82419c98da3be4a014f5e97093774078eb025513
Koldo Pina <koldopinaortiz@gmail.com>
2018-02-02 21:44:46 +0100

    add data file and some code

--- Post 1 - Python Groupby parte 1/post1_code.py
+++ Post 1 - Python Groupby parte 1/post1_code.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python.
+"""
+File name: post_defaultDict.py
+Author: Koldo Pina
+Date created: 31/01/2018
+Date last modified: 31/01/2018
+Python Version: 3.5
+"""
 
+#Agrupando con Pandas
+import pandas as pd
+
+df = pd.read_csv('trabajos_01.csv', sep=";", decimal=",")
+grouped = df.groupby(['DIA_MES'])
+res = grouped['HORAS_TRABAJO'].sum()
+res_df = pd.DataFrame({'DI_MES': res.index, 'HORAS_TRABAJO': res.values})
+
+print (df.head())
+print (df.tail())
