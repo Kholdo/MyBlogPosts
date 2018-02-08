a073aa91b967980b85eeffc5a58fad0abf5f2440
Koldo Pina <koldopinaortiz@gmail.com>
2018-02-08 20:11:29 +0100

    load data rows into a list

---
+++ Post 3 - Python Groupby parte 3/post3_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python.
+"""
+File name: post3.py
+Author: Koldo Pina
+Date created: 08/02/2018
+Date last modified: 08/02/2018
+Python Version: 3.5
+"""
+
+#Cargamos los datos en una lista de diccionarios
+import csv
+
+csv_rdr = csv.reader(open('potencias_frio.csv'), delimiter=';')
+kw_cl_dict = []
+for index, row in enumerate(csv_rdr):
+    if index == 0:
+        encabezado = row
+    else:
+        row[4] = int(row[4])
+        d = {}
+        for index, value in enumerate(row):
+            key = encabezado[index]
+            d[key] = value
+        kw_cl_dict.append(d)
