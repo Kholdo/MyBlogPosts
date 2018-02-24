7cbd72dd35ab0645f351fbd71b04c854a7142aaf
Koldo <koldopinaortizgmail.com>
2018-02-24 10:46:45 +0100

    libraries and reading data

--- Post 5 - Geocodificacion con geopy/post5_code.py
+++ Post 5 - Geocodificacion con geopy/post5_code.py
@@ -5,3 +5,8 @@ Author: Koldo Pina
 Date created: 18/02/2018
 Python Version: 3.5
 """
+
+import pandas as pd
+from geopy.geocoders import GoogleV3
+
+df = pd.read_csv('Py_Lego_stores_2018218.csv', sep=";")
