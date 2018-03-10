171f82378038b5513c7002e6cea24eff6b8065d7
Koldo <koldopinaortiz@gmail.com>
2018-03-10 22:49:16 +0100

    fit

--- Post 6 - Cómo entrenar a tu perceptron/entrenando_perceptron.py
+++ Post 6 - Cómo entrenar a tu perceptron/entrenando_perceptron.py
@@ -64,3 +64,21 @@ class SimplePerceptron():
         """
         output = np.where(self.zeta(X) >= 0.0, 1, 0)
         return output
+
+    def fit(self, X, y):
+        #Ponemos a cero los pesos
+        self.weights = [0] * (X.shape[1] + 1)
+        self.errors = []
+        self.iteraciones = 0
+        while True:
+            errors = 0
+            for features, expected in zip(X,y):
+                delta_weight = self.eta * (expected - self.predict(features))
+                self.weights[1:] += delta_weight * features
+                self.weights[0] += delta_weight * 1
+                errors += int(delta_weight != 0.0)
+            self.errors.append(errors)
+            self.iteraciones += 1
+            if errors == 0:
+                break
+
