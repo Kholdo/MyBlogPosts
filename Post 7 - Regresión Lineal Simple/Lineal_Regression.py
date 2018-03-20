aa1882115b1a61a4561b9d28391bd945dc6ede03
Koldo <koldopinaortizgmail.com>
2018-03-20 20:30:42 +0100

    draw section

--- Post 7 - Regresión Lineal Simple/Lineal_Regression.py
+++ Post 7 - Regresión Lineal Simple/Lineal_Regression.py
@@ -7,6 +7,7 @@ Python Version: 3.5
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Loading data into a dataframe
 df = pd.read_csv('data_LR.csv', sep="|")
@@ -27,5 +28,19 @@ df = df[df['KWIT']>0]
 print(df.describe())
 print('_'*60 + 'NULL VALUES')
 print(df.isnull().sum())
+
+#drawing the data
+X, y = df['TEMPEXT'].values, df['KWCLT'].values
+
+fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+axs[0, 0].scatter(X, y, c='r', edgecolors=(0, 0, 0))
+axs[0, 0].set_title('Scatter X y')
+axs[1, 0].hist(X, color='red')
+axs[1, 0].set_title('Hist X')
+axs[0, 1].hist(y, color='blue')
+axs[0, 1].set_title('Hist y')
+axs[1, 1].hist2d(X, y)
+axs[1, 1].set_title('Hist 2D')
+plt.show()
 
 
