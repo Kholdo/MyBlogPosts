47d53730105af26e7a80f22a8c2bddeb40561bb1
Koldo <koldopinaortizgmail.com>
2018-03-20 17:27:01 +0100

    info section

--- Post 7 - Regresión Lineal Simple/Lineal_Regression.py
+++ Post 7 - Regresión Lineal Simple/Lineal_Regression.py
@@ -8,11 +8,24 @@ Python Version: 3.5
 
 import pandas as pd
 
-#Importando los datos
+#Loading data into a dataframe
 df = pd.read_csv('data_LR.csv', sep="|")
 df = df.replace(',','.', regex=True).astype(float)
-
+#dataframe info section
 print(df.head())
 print(df.tail())
+print('_'*60 + 'COLUMNS')
+print(df.columns.values)
+print('_'*60 + 'SHAPE')
+print(df.shape)
+print('_'*60 + 'INFO')
+print(df.info())
+print('_'*60 + 'DESCRIBE')
+print(df.describe())
+df = df[df['KWCLT']>0]
+df = df[df['KWIT']>0]
+print(df.describe())
+print('_'*60 + 'NULL VALUES')
+print(df.isnull().sum())
 
 
