87f020eb20025e485861b1d4bd605ae367aa7731
Koldo <koldopinaortizgmail.com>
2018-03-20 21:08:13 +0100

    drawing section.

--- Post 7 - Regresión Lineal Simple/Lineal_Regression.py
+++ Post 7 - Regresión Lineal Simple/Lineal_Regression.py
@@ -9,8 +9,9 @@ Python Version: 3.5
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Modelo "cincel y martillo"
+# Modelo "cincel y martillo"
 import csv
+
 with open('data_LR.csv', 'r') as f:
     reader = csv.reader(f, delimiter='|')
     TEMPEXT = []
@@ -24,52 +25,56 @@ with open('data_LR.csv', 'r') as f:
             KWCLT.append(float(row[1].replace(",", ".")))
             data.append([float(row[0].replace(",", ".")), float(row[1].replace(",", "."))])
 X, y = TEMPEXT, KWCLT
-fig, axs = plt.subplots(2, 2, figsize=(8, 8))
-axs[0, 0].scatter(X, y, c='r', edgecolors=(0, 0, 0))
-axs[0, 0].set_title('Scatter KWCLT vs TEMPEXT')
+
+
+fs = 10  # fontsize
+fig, axs = plt.subplots(3, 2, figsize=(6, 6))
+plt.subplots_adjust(top=0.9, bottom=0.1, hspace=0.5, wspace=0.2, left=0.125, right=0.9)
+axs[0, 0].scatter(X, y, c='r', edgecolors=(0, 0, 0), alpha=0.2)
+axs[0, 0].set_title('Scatter KWCLT vs TEMPEXT', fontsize=fs)
 axs[1, 0].hist(X, color='red')
-axs[1, 0].set_title('Hist TEMPEXT')
-axs[0, 1].hist(y, color='blue')
-axs[0, 1].set_title('Hist KWCLT')
-axs[1, 1].hist2d(X, y)
-axs[1, 1].set_title('Hist 2D')
+axs[1, 0].set_title('Hist TEMPEXT', fontsize=fs)
+axs[0, 1].hist2d(X, y)
+axs[0, 1].set_title('Hist 2D', fontsize=fs)
+axs[1, 1].hist(y, color='blue')
+axs[1, 1].set_title('Hist KWCLT', fontsize=fs)
+axs[2, 0].boxplot(X)
+axs[2, 0].set_title('Box TEMPEXT', fontsize=fs)
+axs[2, 1].boxplot(y)
+axs[2, 1].set_title('Box KWCLT', fontsize=fs)
 plt.show()
 
+# Modelo con sklearn
+# Loading data into a dataframe
+# df = pd.read_csv('data_LR.csv', sep="|")
+# df = df.replace(',','.', regex=True).astype(float)
+# dataframe info section
+# print(df.head())
+# print(df.tail())
+# print('_'*60 + 'COLUMNS')
+# print(df.columns.values)
+# print('_'*60 + 'SHAPE')
+# print(df.shape)
+# print('_'*60 + 'INFO')
+# print(df.info())
+# print('_'*60 + 'DESCRIBE')
+# print(df.describe())
+# df = df[df['KWCLT']>0]
+# df = df[df['KWIT']>0]
+# print(df.describe())
+# print('_'*60 + 'NULL VALUES')
+# print(df.isnull().sum())
 
+# drawing the data
+# X, y = df['TEMPEXT'].values, df['KWCLT'].values
 
-#Modelo con sklearn
-#Loading data into a dataframe
-#df = pd.read_csv('data_LR.csv', sep="|")
-#df = df.replace(',','.', regex=True).astype(float)
-#dataframe info section
-#print(df.head())
-#print(df.tail())
-#print('_'*60 + 'COLUMNS')
-#print(df.columns.values)
-#print('_'*60 + 'SHAPE')
-#print(df.shape)
-#print('_'*60 + 'INFO')
-#print(df.info())
-#print('_'*60 + 'DESCRIBE')
-#print(df.describe())
-#df = df[df['KWCLT']>0]
-#df = df[df['KWIT']>0]
-#print(df.describe())
-#print('_'*60 + 'NULL VALUES')
-#print(df.isnull().sum())
-
-#drawing the data
-#X, y = df['TEMPEXT'].values, df['KWCLT'].values
-
-#fig, axs = plt.subplots(2, 2, figsize=(8, 8))
-#axs[0, 0].scatter(X, y, c='r', edgecolors=(0, 0, 0))
-#axs[0, 0].set_title('Scatter X y')
-#axs[1, 0].hist(X, color='red')
-#axs[1, 0].set_title('Hist X')
-#axs[0, 1].hist(y, color='blue')
-#axs[0, 1].set_title('Hist y')
-#axs[1, 1].hist2d(X, y)
-#axs[1, 1].set_title('Hist 2D')
-#plt.show()
-
-
+# fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+# axs[0, 0].scatter(X, y, c='r', edgecolors=(0, 0, 0))
+# axs[0, 0].set_title('Scatter X y')
+# axs[1, 0].hist(X, color='red')
+# axs[1, 0].set_title('Hist X')
+# axs[0, 1].hist(y, color='blue')
+# axs[0, 1].set_title('Hist y')
+# axs[1, 1].hist2d(X, y)
+# axs[1, 1].set_title('Hist 2D')
+# plt.show()
