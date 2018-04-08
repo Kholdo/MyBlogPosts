ad49b5eb8c5f7c0bd81135e445f479b8a8f5e709
Koldo <koldopinaortiz@gmail.com>
2018-04-08 10:12:07 +0200

    importing  train_test_split

--- Post 7 - Regresión Lineal Simple/Lineal_Regression.py
+++ Post 7 - Regresión Lineal Simple/Lineal_Regression.py
@@ -254,6 +254,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 
 slr_df = pd.read_csv('data_LR.csv', sep="|")
