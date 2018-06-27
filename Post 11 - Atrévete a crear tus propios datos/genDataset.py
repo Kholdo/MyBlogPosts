f969096da86f84736d67a443c53649b0f39f3e4b
Koldo <koldopinaortiz@gmail.com>
2018-06-27 19:51:27 +0200

    defining the genDataset function

--- Post 11 - Atrévete a crear tus propios datos/genDataset.py
+++ Post 11 - Atrévete a crear tus propios datos/genDataset.py
@@ -5,3 +5,20 @@ Author: Koldo Pina
 Date created: 27/06/2018
 Python Version: 3.5
 """
+
+
+def genDataset(n_features=4, n_samples=1000, low=0, high=10, weights=[0.25, 0.25, 0.25, 0.25], threshold=5):
+    """
+    The function generate a random dataset with n predictor features and the result. The result
+    can take two values, zero and one. The result depends on the threshold.
+
+    :param n_features: Number of features.
+    :param n_samples: Number of samples.
+    :param low: The lowest value for the range of random values.
+    :param high: The highest value for the range of random values.
+    :param weight: List of the weights for each feature. There must be a weight for each feature.
+    :param threshold: It is the value that marks the limit to assign zero or one to the result.
+
+    :return: Dataframe
+
+    """
