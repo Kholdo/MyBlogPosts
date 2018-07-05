4962db047d817557ca996be971cb3aad3ab9d269
Koldo Pina <koldopinaortiz@gmail.com>
2018-07-05 14:52:32 +0200

    describe_plus function

--- Post 12 - Adaline . The Perceptron Evolution/adaline.py
+++ Post 12 - Adaline . The Perceptron Evolution/adaline.py
@@ -11,3 +11,20 @@ import pandas as pd
 df_adaline = pd.read_csv('adaline_data.csv', sep=';')
 df_perceptron = pd.read_csv('perceptron_data.csv', sep=';')
 
+def describe_plus(df):
+    """
+    Function that returns describe dataframe plus number of NaNs, number of uniques values, the mode
+    # and the sts per column
+    :param df: dataframe
+    :return: dataframe
+    """
+    describe = df.describe()
+    describe.loc['NaNs'] = [df_adaline[feature].isnull().sum() for feature in df.columns]
+    describe.loc['uniques'] = [len(df_adaline[feature].unique()) for feature in df.columns]
+    describe.loc['mode'] = [df_adaline[feature].mode()[0] for feature in df.columns]
+    describe.loc['std'] = [df_adaline[feature].std() for feature in df.columns]
+    return describe.transpose()
+
+print (describe_plus(df_adaline))
+
+print (describe_plus(df_perceptron))
