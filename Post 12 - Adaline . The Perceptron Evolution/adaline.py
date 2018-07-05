308293c06130362462e646884678a3c7e3b1f00d
Koldo Pina <koldopinaortiz@gmail.com>
2018-07-05 14:50:14 +0200

    Importing modules
    load data from csv files

---
+++ Post 12 - Adaline . The Perceptron Evolution/adaline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Koldo Pina
+Date created: 05/07/2018
+Python Version: 3.6
+"""
+
+import pandas as pd
+
+df_adaline = pd.read_csv('adaline_data.csv', sep=';')
+df_perceptron = pd.read_csv('perceptron_data.csv', sep=';')
+
