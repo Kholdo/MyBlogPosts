d3d9468e719ce7718cdf3f8d7649622a24f1ad33
Koldo Pina <koldopinaortiz@gmail.com>
2018-07-09 12:57:17 +0200

    adding plot decision regions function

--- Post 12 - Adaline . The Perceptron Evolution/adaline.py
+++ Post 12 - Adaline . The Perceptron Evolution/adaline.py
@@ -150,3 +150,28 @@ class Adaline_k():
         :return: salida de la neurona
         """
         return np.where(self.activacion(X) >= 0.0, 1, -1)
+
+def plot_decision_regions(X, y, classifier, X_test, y_test, resolution=0.02, test_idx=False):
+    markers = ('s', 'x')
+    colors = ['#D7D98E', '#91CFEA']
+    palette = sns.color_palette(colors)
+    cmap = ListedColormap(colors)
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+            alpha=1, c=cmap(idx),
+            marker=markers[idx], label='%s | train set'%str(cl), linewidths=0.5,edgecolors= 'grey')
+    if test_idx:
+        cmap_test = ListedColormap(colors)
+        for idx, cl in enumerate(np.unique(y_test)):
+            plt.scatter(x=X_test[y_test == cl, 0], y=X_test[y_test == cl, 1],
+                alpha=1, c=cmap_test(idx), linewidths=0.8,
+                marker='o', label='%s | test set'%str(cl), edgecolors= 'black')
