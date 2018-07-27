234d228d1b001a118cf48d3e75617721409fc9f7
koldo <koldopinaortiz@gmail.com>
2018-07-27 10:38:43 +0200

    changing plot design

--- Post 13 - De la regresión lineal a la logística/sigmoide.py
+++ Post 13 - De la regresión lineal a la logística/sigmoide.py
@@ -17,11 +17,20 @@ x = np.arange(-15, 15, 0.5)
 y = sigmoide(x)
 y_mid = [0.5 for ele in x]
 
-fig = plt.figure(figsize=(12,8))
+fig = plt.figure()
+sg = fig.add_subplot(111)
+
+sg.set_title('función sigmoide',fontsize=16, fontweight='bold')
+
+sg.set_xlabel('x')
+sg.set_ylabel('y')
+
+sg.text(-7, 0.9, r'$y=\frac{1}{1+e^{-f(x)}}$', style='italic',fontsize=15,
+        bbox={'facecolor': 'green', 'alpha': 0.3, 'pad': 10})
+
+sg.text(-15, 0.53, r'pto inflexión: y=0.5', fontsize=15, color='red')
+
 plt.plot(x, y, color='#1F5320', lw = 3.0)
 plt.plot(x, y_mid, color='red', lw = 2.0, ls = 'dashed')
-plt.ylabel('Y')
-plt.xlabel('X')
-plt.suptitle('función sigmoide: ' + r'$y=\frac{1}{1+e^{-f(x)}}$', size = 20)
-plt.grid(True)
+
 plt.show()
