4c6e185b23466b554ccd0209823883ac42ec05c5
Koldo <koldopinaortiz@gmail.com>
2018-08-02 19:24:56 +0200

    starting the class with init function

--- new_post_draft/draft.py
+++ new_post_draft/draft.py
@@ -6,3 +6,13 @@ Date created: 02/08/2018
 Python Version: 3.6
 """
 
+class LogReg():
+
+    def __init__(self, error, n_iter):
+        """
+        :param error: error gradient descent
+        :param n_iter: número de iteraciones(epochs)
+        """
+        self.error = eta
+        self.n_iter = n_iter
+
