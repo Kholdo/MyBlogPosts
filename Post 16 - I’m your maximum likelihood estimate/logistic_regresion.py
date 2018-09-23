0e7a512fd44b85330af37deef2fdff960015bb9d
Koldo <koldopinaortiz@gmail.com>
2018-09-23 11:04:21 +0200

    starting post number 16. Import modules

---
+++ Post 16 - I’m your maximum likelihood estimate/logistic_regresion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Koldo Pina
+Date created: 23/09/2018
+Python Version: 3.6
+"""
+
+#Import the modules we are going to need
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, metrics
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+import seaborn as sns
+import matplotlib.pyplot as plt
