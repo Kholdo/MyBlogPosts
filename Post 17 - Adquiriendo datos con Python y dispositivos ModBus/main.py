7ff92d8e3accd562f33835e490286c5d5f1b947a
Koldo <koldopinaortiz@gmail.com>
2018-11-18 18:55:08 +0100

    connection created

--- Post 17 - Adquiriendo datos con Python y dispositivos ModBus/main.py
+++ Post 17 - Adquiriendo datos con Python y dispositivos ModBus/main.py
@@ -9,6 +9,14 @@ parity = step_th()['default_config']['parity']
 baudrate = step_th()['default_config']['baudrate']
 timeout = step_th()['default_config']['timeout']
 nper = step_th()['default_config']['nper']
+port = '/dev/ttyUSB0'
 
 client = ModbusClient(method=method,
-                      stopbits = stopbits)
+                      stopbits=stopbits,
+                      bytesize=bytesize,
+                      parity=parity,
+                      baudrate=baudrate,
+                      timeout=timeout,
+                      port=port)
+
+connection = client.connect()
