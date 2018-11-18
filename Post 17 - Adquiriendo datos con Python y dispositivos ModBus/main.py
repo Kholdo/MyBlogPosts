53f51a58e7cd2eb407d32ee5d9e88dd23612ca0e
Koldo <koldopinaortiz@gmail.com>
2018-11-18 19:30:53 +0100

    reading float registers

--- Post 17 - Adquiriendo datos con Python y dispositivos ModBus/main.py
+++ Post 17 - Adquiriendo datos con Python y dispositivos ModBus/main.py
@@ -23,10 +23,24 @@ connection = client.connect()
 
 if connection:
     # we read first the integer data
-    for key, value in stepTH()['memo_Integers'].items():
+    for key, value in step_th()['memo_Integers'].items():
         rr = client.read_holding_registers(value, 1, unit=0x01)
         if not rr.isError():
             val = rr.registers[0]
             print('{}: {}'.format(key, val))
         else:
             print('{}: error'.format(key))
+
+    for key, value in step_th()['memo_Floats'].items():
+        rr1 = client.read_holding_registers(value, 1, unit=0x01)
+        rr2 = client.read_holding_registers(value, 2, unit=0x01)
+        if not rr1.isError() and not rr2.isError():
+            print('{}: {};{}'.format(key, rr1.registers[0], rr2.registers[0]))
+            bin_number = '0' + str(np.base_repr(rr2.registers[0], base=2)) \
+                         + '0' + str(np.base_repr(rr1.registers[0], base=2))
+            f = int(bin_number, 2)
+            print('{}:{}'.format(key, struct.unpack('f', struct.pack('I', f))[0]))
+        else:
+            print('{}: error'.format(key))
+
+    client.close()
